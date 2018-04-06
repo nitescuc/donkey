a70a37da2d9338caf515a0ce300b7ece5275655c
Will Roscoe <wroscoe@gmail.com>
2018-04-06 19:33:10 +0000

    fixes on actuator and pirfcontroller

--- donkeycar/parts/pirfcontroller.py
+++ donkeycar/parts/pirfcontroller.py
@@ -159,9 +159,10 @@ class PiRfController(object):
             if steering_tx == None:
                 steering_tx = 1500
             change_mode_tx = self._changeModePwm.timeHigh
-            if (change_mode_tx == None or change_mode_tx < 1500) and (self._changeModePwm.timeLow != None and self._changeModePwm.timeLow > 1500):
+#            print(change_mode_tx, self._changeModePwm.timeLow)
+            if (change_mode_tx == None or change_mode_tx < 1500):
                 self._ready_change_mode = True
-            else:
+            elif (self._changeModePwm.timeLow != None and self._changeModePwm.timeLow > 1500):
                 if self._ready_change_mode:
                     self.next_mode()
                 self._ready_change_mode = False
