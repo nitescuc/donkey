94723a84213e4f8f65153918d52536ee0c0b8403
NITESCU Cristian <cristian.nitescu@renault.com>
2018-10-22 23:41:56 +0200

    Back to pygame webcam

--- donkeycar/parts/camera.py
+++ donkeycar/parts/camera.py
@@ -73,6 +73,59 @@ class PiCamera(BaseCamera):
 
 class Webcam(BaseCamera):
     def __init__(self, resolution = (160, 120), framerate = 20):
+        import pygame
+        import pygame.camera
+
+        super().__init__()
+
+        pygame.init()
+        pygame.camera.init()
+        l = pygame.camera.list_cameras()
+        self.cam = pygame.camera.Camera(l[0], resolution, "RGB")
+        self.resolution = resolution
+        self.cam.start()
+        self.framerate = framerate
+
+        # initialize variable used to indicate
+        # if the thread should be stopped
+        self.frame = None
+        self.on = True
+
+        print('WebcamVideoStream loaded.. .warming camera')
+
+        time.sleep(2)
+
+    def update(self):
+        from datetime import datetime, timedelta
+        import pygame.image
+        while self.on:
+            start = datetime.now()
+
+            if self.cam.query_image():
+                # snapshot = self.cam.get_image()
+                # self.frame = list(pygame.image.tostring(snapshot, "RGB", False))
+                snapshot = self.cam.get_image()
+                snapshot1 = pygame.transform.scale(snapshot, self.resolution)
+                self.frame = pygame.surfarray.pixels3d(pygame.transform.rotate(pygame.transform.flip(snapshot1, True, False), 90))
+
+            stop = datetime.now()
+            s = 1 / self.framerate - (stop - start).total_seconds()
+            if s > 0:
+                time.sleep(s)
+
+        self.cam.stop()
+
+    def run_threaded(self):
+        return self.frame
+
+    def shutdown(self):
+        # indicate that the thread should be stopped
+        self.on = False
+        print('stoping Webcam')
+        time.sleep(.5)
+
+class CV2Webcam(BaseCamera):
+    def __init__(self, resolution = (160, 120), framerate = 20):
         super().__init__()
 
         cam = cv2.VideoCapture(0)
