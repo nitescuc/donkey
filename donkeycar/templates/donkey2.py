103a65f417aedabd8728c26b1968442bd8038a09
NITESCU Cristian <cristian.nitescu@renault.com>
2018-11-06 19:21:00 +0100

    Fix camera parameters

--- donkeycar/templates/donkey2.py
+++ donkeycar/templates/donkey2.py
@@ -45,7 +45,7 @@ from donkeycar.parts.led_display import LedDisplay
 
 from sys import platform
 
-def drive(cfg, model_path=None, use_joystick=False, use_tx=False, use_pirf=False, use_sonar=False):
+def drive(cfg, model_path=None, use_joystick=False, use_tx=False, use_pirf=False, use_sonar=False, use_fpv=False):
     '''
     Start the drive loop
     Each part runs as a job in the Vehicle loop, calling either
@@ -60,7 +60,7 @@ def drive(cfg, model_path=None, use_joystick=False, use_tx=False, use_pirf=False
     V = dk.vehicle.Vehicle()
 
     if platform != "darwin":
-        cam = PiCamera(resolution=cfg.CAMERA_RESOLUTION, camera.CAMERA_FRAMERATE)
+        cam = PiCamera(resolution=cfg.CAMERA_RESOLUTION, framerate=cfg.CAMERA_FRAMERATE)
         V.add(cam, outputs=['cam/image_array'], threaded=True)
 
     steering_controller = PCA9685(cfg.STEERING_CHANNEL)
