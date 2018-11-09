735b463b71fb71c56bcb29a95ba267aa79f71dd1
NITESCU Cristian <cristian.nitescu@renault.com>
2018-11-09 17:07:45 +0100

    Text prints

--- donkeycar/parts/camera.py
+++ donkeycar/parts/camera.py
@@ -54,9 +54,21 @@ class PiCamera(BaseCamera):
             yLen = resolution[1]
             xLen = resolution[0]
             for x in range(0, yLen, yLen//12):
-                cv2.line(img, (0,x), (xLen,x), (57, 255, 20), 1)
+                cv2.line(img, (40,x), (xLen,x), (57, 255, 20), 1)
+                cv2.putText(img, str(x//4),
+                   (0,x+5),
+                   cv2.FONT_HERSHEY_SIMPLEX,
+                   0.5,
+                   (57, 255, 20),
+                   1)
             for x in range(yLen//24, yLen, yLen//12):
-                cv2.line(img, (0,x), (xLen,x), (255, 57, 20), 1)
+                cv2.line(img, (40,x), (xLen,x), (255, 57, 20), 1)
+                cv2.putText(img, str(x//4),
+                   (0,x+5),
+                   cv2.FONT_HERSHEY_SIMPLEX,
+                   0.5,
+                   (255, 57, 20),
+                   1)
 
         return img
 
