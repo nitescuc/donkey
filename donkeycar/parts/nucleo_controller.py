f273c351d96e8481de4ec66b575ab800d4ac7461
Cristian Nitescu <nitescuc@hotmail.com>
2018-11-30 11:24:09 +0000

    Working controller and model

--- donkeycar/parts/nucleo_controller.py
+++ donkeycar/parts/nucleo_controller.py
@@ -7,84 +7,76 @@ class NucleoController(object):
                  serial_device=None,
                  serial_baud=115200,
                  model_path=None,
-                 verbose = False
+                 verbose = False,
+                 limit = 0x04
                  ):
         self.model_path = model_path
         self.serial_device = serial_device
         self.serial_baud = serial_baud
         self.recording = False
+        self.limit = limit
+        self.echo = False
+        self.serialInitialized = False
 
         self.mode = None
-        
-        self.init()
 
-    def init(self):
-        self.serial = serial.Serial(self.serial_device, self.serial_baud)
-        # limit
-        #self.serial.write(b'L0.600')
-        self.change_mode('user')
-        return True
-    
-    def set_mode(self, level):
-        if self.model_path is not None:
-            if level > 0.500:
-                self.mode = 'local'
-            else:
-                self.mode = 'local_angle'
-            self.recording = False
+        self.serial_init()
+
+    def serial_init(self):
+        self.serial = serial.Serial(self.serial_device, self.serial_baud, timeout=5)
+        self.serial.reset_input_buffer()
+        # set limit
+        self.serial.write(b'L')
+        packet = bytearray()
+        packet.append(self.limit)
+        self.serial.write(packet)
+        #
+        print('Serial initialized')
+        self.serialInitialized = True
     
     def change_mode(self, mode):
-        mode_letter = 'mu'
+        mode_letter = 'u'
         if mode == 'local_angle':
-            mode_letter = 'ma'
+            mode_letter = 'a'
         if mode == 'local':
-            mode_letter = 'mp'
+            mode_letter = 'p'
+        self.serial.write(b'm')
         self.serial.write(mode_letter.encode())
         self.mode = mode
 
     def update(self):
-#        while True:
-#            if self.mode != 'user':
-#                self.serial.write(b'r')
-#                line = self.serial.readline()
-#                steering = float(line[:5])
-#                throttle = float(line[5:10]) - 0.5
-#                remote = float(line[10:15])
-                #self.set_mode(remote)
-#            time.sleep(0.5)
-                
         return True
 
     def run_threaded(self, p_angle, p_throttle, p_mode):
+        return True
+
+    def run(self, p_angle, p_throttle, p_mode):
+        if not self.serialInitialized:
+            return 7, 7, 'user', False
         if p_mode != self.mode:
             self.change_mode(p_mode)
         if self.mode != 'user':
             steering = p_angle
             throttle = p_throttle
-            if throttle != None and throttle > 0.2:
-                throttle = 0.2
         if self.mode == 'user':
             self.serial.write(b'r')
-            line = self.serial.readline()
-            steering = float(line[:5]) * 2 - 1
-            throttle = float(line[5:10]) * 2 - 1
-            #remote = float(line[10:15])
-            #
-            if throttle < 0.05 and throttle > -0.05: 
-                throttle = 0
-            #self.set_mode(remote)
-            self.recording = throttle > 0 and self.mode == 'user'
+            temp = self.serial.read(1)
+            if len(temp):
+                steering = temp[0] >> 4
+                throttle = temp[0] & 0x0F
+            else:
+               steering = 7
+               throttle = 7
+            self.recording = throttle > 7 and self.mode == 'user'
         else:
             if throttle != None and steering != None:
-                steering = (steering + 1)/2
-                throttle = (throttle + 1)/2
-                print(throttle)
-                self.serial.write(('w{:01.3f}{:01.3f}'.format(steering, throttle)).encode())
+                package = bytearray()
+                package.append(steering * 16 + throttle)
+                self.serial.write(b'w')
+                self.serial.write(package)
         
         return steering, throttle, self.mode, self.recording
 
-    def run(self, angle, steering):
-        return False
-
     def shutdown(self):
+        self.change_mode('user')
         self.running = False
