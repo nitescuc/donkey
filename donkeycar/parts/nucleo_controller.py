2833730c4c6f3e386e1683d67a0c916a9d406348
NITESCU Cristian <cristian.nitescu@renault.com>
2019-03-29 00:44:14 +0100

    Added break management

--- donkeycar/parts/nucleo_controller.py
+++ donkeycar/parts/nucleo_controller.py
@@ -1,5 +1,6 @@
 import serial
 import time
+from collections import deque
 
 class NucleoController(object):
 
@@ -21,10 +22,15 @@ class NucleoController(object):
         self.fast_throttle = fast_throttle
         self.echo = False
         self.serialInitialized = False
+        
+        self.speed = 0
+        self.breakSeq = deque([])
+        self.throttleMap = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, self.slow_throttle, 11, self.medium_throttle, 13, self.fast_throttle]
 
         self.mode = None
 
         self.serial_init()
+    
 
     def apply_config(self, config):
         if 'limit' in config and config['limit']:
@@ -33,6 +39,7 @@ class NucleoController(object):
             packet = bytearray()
             packet.append(self.limit)
             self.serial.write(packet)
+
 
     def serial_init(self):
         self.serial = serial.Serial(self.serial_device, self.serial_baud, timeout=5)
@@ -46,6 +53,7 @@ class NucleoController(object):
         print('Serial initialized')
         self.serialInitialized = True
     
+
     def change_mode(self, mode):
         mode_letter = 'u'
         if mode == 'local_angle':
@@ -56,11 +64,14 @@ class NucleoController(object):
         self.serial.write(mode_letter.encode())
         self.mode = mode
 
+
     def update(self):
         return True
 
+
     def run_threaded(self, p_angle, p_throttle, p_mode):
         return True
+
 
     def run(self, p_angle, p_throttle, p_mode, p_recording):
         if not self.serialInitialized:
@@ -81,9 +92,19 @@ class NucleoController(object):
                throttle = 7
         else:
             if throttle != None and steering != None:
-                throttleMap = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, self.slow_throttle, 11, self.medium_throttle, 13, self.fast_throttle]
+                # break management
+                if throttle >= 12:
+                    self.speed = self.speed + 1
+                elif throttle < 12:
+                    print('Speed: ' + str(self.speed))
+                    if self.speed > 10:
+                        self.breakSeq.extend([0,0])
+                    self.speed = 0
+                if len(self.breakSeq):
+                    throttle = self.breakSeq.popleft()
+                # send throttle
                 package = bytearray()
-                package.append(steering * 16 + throttleMap[throttle])
+                package.append(steering * 16 + self.throttleMap[throttle])
                 self.serial.write(b'w')
                 self.serial.write(package)
             steering = 7
@@ -92,6 +113,7 @@ class NucleoController(object):
         self.recording = p_recording or (throttle > 7 and self.mode == 'user')
         
         return steering, throttle, self.mode, self.recording
+
 
     def shutdown(self):
         self.change_mode('user')
