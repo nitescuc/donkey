669fd5576081a5388fe6acf5307996007cfc7f8f
NITESCU Cristian <cristian.nitescu@renault.com>
2019-05-06 21:14:16 +0200

    Print speed for debugging

--- donkeycar/parts/zmq_speed_sensor.py
+++ donkeycar/parts/zmq_speed_sensor.py
@@ -1,6 +1,7 @@
 import sys
 import zmq
 import numpy as np
+import time
 
 class ZmqSpeedSensor():
     def __init__(self, remote):
@@ -12,6 +13,8 @@ class ZmqSpeedSensor():
         self.subscriber.setsockopt(zmq.SUBSCRIBE, b"speed")
 
         self.on = True
+
+        self.last_time = time.time() + 5
 
     def run(self):
         [address, speed] = self.subscriber.recv_multipart()
@@ -25,6 +28,10 @@ class ZmqSpeedSensor():
         while self.on:
             [address, speed] = self.subscriber.recv_multipart()
             self.speed = float(speed)/1000
+            tt = time.time()
+            if self.last_time < tt:
+                self.last_time = tt + 5
+                print('Speed ' + str(self.speed))
 
     def shutdown(self):
         # indicate that the thread should be stopped
